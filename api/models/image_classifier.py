b6e6123b80b1ba40b187f25ce7307f35bd7f7535
antarr <antarr.byrd@gmail.com>
2023-01-29 13:47:44 -0600

    Use OpenAI for Image Classification
    Move Images in Common path
    Set python to 3.10 to support OpenAI
    Update ImageClassifier

--- api/models/image_classifier.py
+++ api/models/image_classifier.py
@@ -1,35 +1,30 @@
-import pandas as pd
+from imageai.Classification import ImageClassification
+import os
 from pathlib import Path
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 
-def load_and_train():
-    toys_path = Path('images/toys')
-    foods_path = Path('images/foods')
-    toys = [str(path) for path in toys_path.glob('*.jpg')]
-    foods = [str(path) for path in foods_path.glob('*.jpg')]
-    training_data = []
-    training_labels = []
-    for img in toys:
-        training_data.append(img)
-        training_labels.append('toy')
-    for img in foods:
-        training_data.append(img)
-        training_labels.append('food')
-    df = pd.DataFrame(training_data, columns=['image'])
-    df['labels'] = training_labels
-    df = df.sample(frac=1).reset_index(drop=True)
-    return df
+class ImageClassifier():
+    def __init__(self):
+        self.classifier = ImageClassification()
+        self.classifier.setModelTypeAsResNet50()
+        self.classifier.setModelPath(os.path.join(os.getcwd(), "pretrained", "resnet50-19c8e357.pth"))
+        self.classifier.loadModel()
+
+    def classify(self, image_path):
+        predictions, probabilities = self.classifier.classifyImage(image_path, result_count=5)
+        classifations = []
+        for index in range(len(predictions)):
+            classifations.append({'prediction': predictions[index], 'probability': probabilities[index]})
+        return classifations
 
 
+# For testing purposes
 if __name__ == '__main__':
-    trained_model = load_and_train()
-
-    # Save trained model as csv and hdf files
-    trained_model.to_csv('examples/trained_model.csv', index=False)
-    trained_model.to_hdf('examples/trained_model.h5', key='df', mode='w')
-
-    # Create a countplot of labels in the trained model and save it as a png file
-    sns.countplot(x='labels', data=trained_model)
-    plt.savefig('examples/plot.png')
+    images_path = Path("images/")
+    images = [str(path) for path in images_path.glob('*.jpg')]
+    image_classifier = ImageClassifier()
+    for image in images:
+        print(image)
+        classifictions = image_classifier.classify(image)
+        print(classifictions)
+        print('-----------------')
